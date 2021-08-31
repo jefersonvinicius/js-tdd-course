c7289fb7e1f1ab0de9a256d08a1e6766bb3b8172
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-08-31 09:35:38 -0300

    Convert to es6

--- 04-tests-in-pratice/src/main.js
+++ 04-tests-in-pratice/src/main.js
@@ -1,15 +1,17 @@
-module.exports = {
-  sum: (a, b) => a + b,
-  sub: (a, b) => a - b,
-  mult: (a, b) => a * b,
-  div(a, b) {
-    if (b === 0) throw new this.DivisionByZero();
-    return a / b;
-  },
-  DivisionByZero: class DivisionByZero extends Error {
-    constructor() {
-      super("It's not possible divide by zero");
-      this.name = this.constructor.name;
-    }
-  },
+export class DivisionByZero extends Error {
+  constructor() {
+    super("It's not possible divide by zero");
+    this.name = this.constructor.name;
+  }
+}
+
+export const sum = (a, b) => a + b;
+
+export const sub = (a, b) => a - b;
+
+export const mult = (a, b) => a * b;
+
+export const div = (a, b) => {
+  if (b === 0) throw new DivisionByZero();
+  return a / b;
 };
