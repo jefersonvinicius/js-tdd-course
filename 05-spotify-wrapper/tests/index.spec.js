3e36bb163bd6f5389c5503b61f2b8aff22327742
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-09-15 09:03:45 -0300

    Creating method request

--- 05-spotify-wrapper/tests/index.spec.js
+++ 05-spotify-wrapper/tests/index.spec.js
@@ -1,4 +1,11 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+chai.use(sinonChai);
+
+global.fetch = require('node-fetch');
+
 import SpotifyWrapper, { TokenNotProvided } from '../src/index';
 
 describe('SpotifyWrapper', () => {
@@ -28,5 +35,48 @@ describe('SpotifyWrapper', () => {
   it('should use the default apiURL if it not provided', () => {
     const spotify = new SpotifyWrapper({ token: 'any' });
     expect(spotify.apiURL).to.be.equal('https://api.spotify.com/v1');
+  });
+
+  describe('request method', () => {
+    let fetchStub;
+    let fetchPromise;
+
+    beforeEach(() => {
+      fetchPromise = sinon.promise();
+      fetchStub = sinon.stub(global, 'fetch').returns(fetchPromise);
+    });
+
+    afterEach(() => {
+      fetchStub.restore();
+    });
+
+    it('should request method exists', () => {
+      const spotify = new SpotifyWrapper({ token: 'any' });
+      expect(spotify.request).to.exist;
+    });
+
+    it('should call fetch', () => {
+      const spotify = new SpotifyWrapper({ token: 'any' });
+      spotify.request();
+      expect(fetchStub).to.be.calledOnce;
+    });
+
+    it('should call fetch with correct url', () => {
+      const spotify = new SpotifyWrapper({ token: 'any' });
+      spotify.request('any_url');
+      expect(fetchStub).to.have.been.calledWith('any_url');
+    });
+
+    it('should call fetch with correct header', () => {
+      const token = 'any';
+      const spotify = new SpotifyWrapper({ token });
+      const expectedHeaders = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      spotify.request('any_url');
+      expect(fetchStub).to.have.been.calledWith('any_url', expectedHeaders);
+    });
   });
 });
