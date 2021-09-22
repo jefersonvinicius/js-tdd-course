f95d70d9b435ef006d771b53051b103b9bae343f
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-09-22 08:53:07 -0300

    Rendering albums list on html

--- 06-spotify-player/src/Spotify.js
+++ 06-spotify-player/src/Spotify.js
@@ -1,7 +1,8 @@
 import SpotifyWrapper from 'spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
-  token: 'any',
+  token:
+    'BQCTt0mm4P0mrkRIvgMUTf8dO674ziQe0Sc4Ov8PtlhLr8vqkvrNTOU0MfQd8miXqzkSxMti6W-fzf2f1yPAm9zSHjX8TkzUqfa0IQxavmQ0yNdcal_hSYQraDaoWkntAdu_wfjUdoQGbN9XINeTPuHhowx6fsE759k',
 });
 
 export default spotify;
--- 06-spotify-player/src/main.js
+++ 06-spotify-player/src/main.js
@@ -1,1 +1,7 @@
-console.log('Hello');
+import renderAlbums from './AlbumList';
+import spotify from './Spotify';
+
+const albums = spotify.search.albums('J Balvin');
+const albumsList = document.getElementById('album-list');
+
+albums.then((data) => renderAlbums(data.albums.items, albumsList));
