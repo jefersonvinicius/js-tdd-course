529be00005449f10ebf8a2c10a5e3f6b57de7465
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-09-22 09:09:44 -0300

    Redering album info

--- 06-spotify-player/src/main.js
+++ 06-spotify-player/src/main.js
@@ -1,7 +1,12 @@
 import renderAlbums from './AlbumList';
+import renderAlbumInfo from './AlbumInfo';
 import spotify from './Spotify';
 
 const albums = spotify.search.albums('J Balvin');
 const albumsList = document.getElementById('album-list');
+const albumInfo = document.getElementById('album-info');
+
+const album = spotify.album.getAlbum('11GmvpYnbgK0rSryPaV5BP');
 
 albums.then((data) => renderAlbums(data.albums.items, albumsList));
+album.then((data) => renderAlbumInfo(data, albumInfo));
