046d7dca1a6770d36ad068c60c082b0dd7b2d3b4
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-09-23 08:46:22 -0300

    Add album tracks to layout

--- 06-spotify-player/src/main.js
+++ 06-spotify-player/src/main.js
@@ -1,12 +1,17 @@
 import renderAlbums from './AlbumList';
 import renderAlbumInfo from './AlbumInfo';
 import spotify from './Spotify';
+import renderAlbumTracks from './AlbumTracks';
 
-const albums = spotify.search.albums('J Balvin');
 const albumsList = document.getElementById('album-list');
 const albumInfo = document.getElementById('album-info');
+const albumTracks = document.getElementById('album-musics');
+
+const albums = spotify.search.albums('J Balvin');
+albums.then((data) => renderAlbums(data.albums.items, albumsList));
 
 const album = spotify.album.getAlbum('11GmvpYnbgK0rSryPaV5BP');
-
-albums.then((data) => renderAlbums(data.albums.items, albumsList));
-album.then((data) => renderAlbumInfo(data, albumInfo));
+album.then((data) => {
+  renderAlbumInfo(data, albumInfo);
+  renderAlbumTracks(data.tracks.items, albumTracks);
+});
