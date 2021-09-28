6b62a60c2a1b7fd2a01ccdb1611afd7053e29fbc
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-09-28 09:36:27 -0300

    Converting to available currencies

--- 07-btc-converter/src/ConvertBTC.js
+++ 07-btc-converter/src/ConvertBTC.js
@@ -1,3 +1,22 @@
-exports.convertBTC = (currency = 'USD', amount = 1) => {
-  return `${amount} BTC to ${currency} = 2000.00`;
+const axios = require('axios').default;
+
+const api = axios.create({
+  baseURL: 'https://api.alternative.me/v2',
+});
+
+const AVAILABLE_CURRENCIES = ['USD', 'EUR', 'GBP', 'RUB', 'JPY', 'CAD', 'KRW', 'PLN'];
+
+class InvalidCurrency extends Error {
+  constructor(currency) {
+    super(`Expected currency be one of ${AVAILABLE_CURRENCIES.join(', ')}. But got ${currency}`);
+  }
+}
+
+exports.convertBTC = async (currency = 'USD', amount = 1) => {
+  if (!AVAILABLE_CURRENCIES.includes(currency)) throw new InvalidCurrency(currency);
+  const { data } = await api.get('/ticker/bitcoin');
+  const btcValue = data.data[1].quotes[currency].price;
+  return `${amount} BTC to ${currency} = ${(btcValue * amount).toFixed(2)}`;
 };
+
+exports.InvalidCurrency = InvalidCurrency;
--- 07-btc-converter/tests/ConvertBTC.spec.js
+++ 07-btc-converter/tests/ConvertBTC.spec.js
@@ -1,12 +1,73 @@
-const { expect } = require('chai');
-const { convertBTC } = require('../src/ConvertBTC');
+const nock = require('nock');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const { convertBTC, InvalidCurrency } = require('../src/ConvertBTC');
+
+chai.use(chaiAsPromised);
+
+const { expect } = chai;
 
 describe('ConvertBTC', () => {
-  it('should return default values when no params are provided', () => {
-    expect(convertBTC()).to.be.equal('1 BTC to USD = 2000.00');
+  const responseMock = {
+    data: {
+      1: {
+        id: 1,
+        name: 'Bitcoin',
+        symbol: 'BTC',
+        website_slug: 'bitcoin',
+        rank: 1,
+        circulating_supply: 18828606,
+        total_supply: 18828606,
+        max_supply: 21000000,
+        quotes: {
+          USD: {
+            price: 42105.0,
+            volume_24h: 31017848033,
+            market_cap: 791332372796,
+            percentage_change_1h: 0.38562094398141,
+            percentage_change_24h: -4.05680757991651,
+            percentage_change_7d: -1.92791383923147,
+            percent_change_1h: 0.38562094398141,
+            percent_change_24h: -4.05680757991651,
+            percent_change_7d: -1.92791383923147,
+          },
+          EUR: {
+            price: 36012.4065,
+            volume_24h: 26529565422.6249,
+            market_cap: 676826578452.419,
+            percent_change_1h: 0.38562094398141,
+            percent_change_24h: -4.05680757991651,
+            percent_change_7d: -1.92791383923147,
+          },
+        },
+        last_updated: 1632830324,
+      },
+    },
+    metadata: {
+      timestamp: 1632830324,
+      num_cryptocurrencies: 3105,
+      error: null,
+    },
+    isFalse: true,
+  };
+
+  it('should use USD and 1 as default values', async () => {
+    nock('https://api.alternative.me/v2').get('/ticker/bitcoin').reply(200, responseMock);
+    expect(await convertBTC()).to.be.equal('1 BTC to USD = 42105.00');
   });
 
-  it('should return correct current and amount', () => {
-    expect(convertBTC('BRL', 10)).to.be.equal('10 BTC to BRL = 2000.00');
+  it('should use USD and 3 as parameters values', async () => {
+    nock('https://api.alternative.me/v2').get('/ticker/bitcoin').reply(200, responseMock);
+    expect(await convertBTC('USD', 3)).to.be.equal('3 BTC to USD = 126315.00');
+  });
+
+  it('should use EUR and 3 as parameters values', async () => {
+    nock('https://api.alternative.me/v2').get('/ticker/bitcoin').reply(200, responseMock);
+    expect(await convertBTC('EUR', 3)).to.be.equal('3 BTC to EUR = 108037.22');
+  });
+
+  it('should get InvalidCurrency when currency is"nt available', async () => {
+    nock('https://api.alternative.me/v2').get('/ticker/bitcoin').reply(200, responseMock);
+    expect(convertBTC('invalid_currency', 3)).to.eventually.rejectedWith(new InvalidCurrency('invalid_currency'));
   });
 });
