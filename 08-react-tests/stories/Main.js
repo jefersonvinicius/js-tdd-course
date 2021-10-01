33d15e779df6c16e05088eef62a6889acdc1f872
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-10-01 08:46:44 -0300

    Add stories

--- 08-react-tests/stories/Main.js
+++ 08-react-tests/stories/Main.js
@@ -1,11 +1,49 @@
-import React from 'react';
-import Component from '../src/Main'; // This is our component
-import { storiesOf } from '@storybook/react';
+import React from "react";
+import FullHeader from "../src/Main";
+import { storiesOf } from "@storybook/react";
 
-storiesOf('Component', module)
-    .add('with lyef name', () => (
-        <Component name="lyef" />
+storiesOf("FullHeader", module)
+    .add("with title", () => <FullHeader title="TDD" />)
+    .add("with title and subtitle", () => (
+        <FullHeader title="TDD" subtitle="Curso de JS com TDD na prática" />
     ))
-    .add('with another name', () => (
-        <Component name="another" />
+    .add("with title, subtitle, bgColor", () => (
+        <FullHeader
+            title="TDD"
+            subtitle="Curso de JS com TDD na prática"
+            bgColor="#3299BB"
+        />
     ))
+    .add("with title, subtitle, bgColor, textColor", () => (
+        <FullHeader
+            title="TDD"
+            subtitle="Curso de JS com TDD na prática"
+            bgColor="#3299BB"
+            textColor="#FF9900"
+        />
+    ))
+    .add("with title, subtitle, bgColor, textColor, font", () => (
+        <FullHeader
+            title="TDD"
+            subtitle="Curso de JS com TDD na prática"
+            bgColor="#3299BB"
+            textColor="#FF9900"
+            font="cursive"
+        />
+    ))
+    .add("with title, subtitle, bgImg", () => (
+        <FullHeader
+            title="TDD"
+            subtitle="Curso de JS com TDD na prática"
+            bgImg="https://raw.githubusercontent.com/willianjusten/photo-example/master/iceland-glacier.jpg"
+        />
+    ))
+    .add("with title, subtitle, video", () => (
+        <FullHeader
+            title="TDD"
+            subtitle="Curso de JS com TDD na prática"
+            bgColor="#EBE9EB"
+            textColor="#3299BB"
+            video="http://callmenick.com/_development/video/full-screen-background-video/media/demo.mp4"
+        />
+    ));
