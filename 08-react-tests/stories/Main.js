3782904324cf67f58108cf643fae9cd5cfbb1b7c
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-10-01 08:54:26 -0300

    Add header tag

--- 08-react-tests/stories/Main.js
+++ 08-react-tests/stories/Main.js
@@ -1,20 +1,20 @@
-import React from "react";
-import FullHeader from "../src/Main";
-import { storiesOf } from "@storybook/react";
+import React from 'react';
+import FullHeader from '../src/Main';
+import { storiesOf } from '@storybook/react';
 
-storiesOf("FullHeader", module)
-    .add("with title", () => <FullHeader title="TDD" />)
-    .add("with title and subtitle", () => (
+storiesOf('FullHeader', module)
+    .add('with title', () => <FullHeader title="TDD" />)
+    .add('with title and subtitle', () => (
         <FullHeader title="TDD" subtitle="Curso de JS com TDD na prática" />
     ))
-    .add("with title, subtitle, bgColor", () => (
+    .add('with title, subtitle, bgColor', () => (
         <FullHeader
             title="TDD"
             subtitle="Curso de JS com TDD na prática"
             bgColor="#3299BB"
         />
     ))
-    .add("with title, subtitle, bgColor, textColor", () => (
+    .add('with title, subtitle, bgColor, textColor', () => (
         <FullHeader
             title="TDD"
             subtitle="Curso de JS com TDD na prática"
@@ -22,7 +22,7 @@ storiesOf("FullHeader", module)
             textColor="#FF9900"
         />
     ))
-    .add("with title, subtitle, bgColor, textColor, font", () => (
+    .add('with title, subtitle, bgColor, textColor, font', () => (
         <FullHeader
             title="TDD"
             subtitle="Curso de JS com TDD na prática"
@@ -31,14 +31,14 @@ storiesOf("FullHeader", module)
             font="cursive"
         />
     ))
-    .add("with title, subtitle, bgImg", () => (
+    .add('with title, subtitle, bgImg', () => (
         <FullHeader
             title="TDD"
             subtitle="Curso de JS com TDD na prática"
             bgImg="https://raw.githubusercontent.com/willianjusten/photo-example/master/iceland-glacier.jpg"
         />
     ))
-    .add("with title, subtitle, video", () => (
+    .add('with title, subtitle, video', () => (
         <FullHeader
             title="TDD"
             subtitle="Curso de JS com TDD na prática"
